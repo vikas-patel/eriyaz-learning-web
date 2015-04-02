a2bc3d55817bb58d6c366468a20a105a0daa00cb
Ashwin Jain <ashvyn.jain@gmail.com>
2015-04-02 18:33:47 +0530

    calculate tanpura pitchshift once upfront

--- client/public/src/er-libs/tanpura/stringsynth.js
+++ client/public/src/er-libs/tanpura/stringsynth.js
@@ -1,4 +1,4 @@
-define(['currentaudiocontext'],function(CurrentAudioContext) {
+define(['currentaudiocontext'], function(CurrentAudioContext) {
 	var context = CurrentAudioContext.getInstance();
 	var StringSynth = function(noteNum, soundbank) {
 		var buffer;
@@ -39,22 +39,42 @@ define(['currentaudiocontext'],function(CurrentAudioContext) {
 			}
 		};
 
-		this.pluck = function(node) {
-			var filter = new SimpleFilter(this.source, this.st);
+		var filter = new SimpleFilter(this.source, this.st);
+		var outSamples = new Float32Array(buffer.length * 2);
+		var framesExtracted = filter.extract(outSamples, buffer.length);
+		var outBuffer = context.createBuffer(2, buffer.length, buffer.sampleRate);
+		// console.log(outSamples);
+		var l = outBuffer.getChannelData(0);
+		var r = outBuffer.getChannelData(1);
+		if (framesExtracted === 0) {
+			// pause();
+		}
+		for (var i = 0; i < framesExtracted; i++) {
+			l[i] = outSamples[i * 2];
+			r[i] = outSamples[i * 2 + 1];
+		}
 
-			this.node.onaudioprocess = function(e) {
-				var l = e.outputBuffer.getChannelData(0);
-				var r = e.outputBuffer.getChannelData(1);
-				var framesExtracted = filter.extract(samples, BUFFER_SIZE);
-				if (framesExtracted === 0) {
-					// pause();
-				}
-				for (var i = 0; i < framesExtracted; i++) {
-					l[i] = samples[i * 2];
-					r[i] = samples[i * 2 + 1];
-				}
-			};
-			this.node.connect(node);
+		this.pluck = function(node) {
+			// var filter = new SimpleFilter(this.source, this.st);
+
+			// this.node.onaudioprocess = function(e) {
+			// 	var l = e.outputBuffer.getChannelData(0);
+			// 	var r = e.outputBuffer.getChannelData(1);
+			// 	var framesExtracted = filter.extract(samples, BUFFER_SIZE);
+			// 	if (framesExtracted === 0) {
+			// 		// pause();
+			// 	}
+			// 	for (var i = 0; i < framesExtracted; i++) {
+			// 		l[i] = samples[i * 2];
+			// 		r[i] = samples[i * 2 + 1];
+			// 	}
+			// };
+			// this.node.connect(node);
+			
+			var source = context.createBufferSource();
+			source.buffer = outBuffer;
+			source.connect(node);
+			source.start();
 		};
 	};
 	return StringSynth;
