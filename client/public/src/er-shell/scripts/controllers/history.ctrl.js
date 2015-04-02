0a00a491829342bb8477f7ba25a47cffad9f3c4d
Ashwin Jain <ashvyn.jain@gmail.com>
2015-04-02 11:54:27 +0530

    resolve - viewing my score after students score

--- client/public/src/er-shell/scripts/controllers/history.ctrl.js
+++ client/public/src/er-shell/scripts/controllers/history.ctrl.js
@@ -1,10 +1,12 @@
   define(['./module'], function(app) {
-  	app.controller("HistoryCtrl", function($scope, $window, Score,$stateParams,$state) {
-  		var usrId = $state.current.name==='s_history'? $stateParams.s_id : $window.localStorage.userId;
-  		$scope.sid = $stateParams.s_id;
-  		$scope.dailyScore = Score.query({
-  			id: usrId
+  	app.controller("HistoryCtrl", function($scope, $window, Score, $stateParams, $state) {
+  		$scope.$watch(function() {
+  			return $stateParams.s_id;
+  		}, function() {
+  			var usrId = $state.current.name === 's_history' ? $stateParams.s_id : $window.localStorage.userId;
+  			$scope.dailyScore = Score.query({
+  				id: usrId
+  			});
   		});
-
   	});
   });
