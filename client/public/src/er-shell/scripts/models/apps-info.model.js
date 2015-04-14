9d69f974cef8550317dbc6b9574049bc93e07df5
Ashwin Jain <ashvyn.jain@gmail.com>
2015-04-14 10:27:44 +0530

    ability to hide apps - for sandboxing

--- client/public/src/er-shell/scripts/models/apps-info.model.js
+++ client/public/src/er-shell/scripts/models/apps-info.model.js
@@ -7,35 +7,40 @@
         desc: 'Do step by step guided singing exercises from easy to difficult.',
         href: '#alankars',
         appUrl: 'er-apps/SingGraph/main.html',
-        aspectRatio: 41 / 20
+        aspectRatio: 41 / 20,
+        show:true
       }, {
         name: 'Freestyle',
         thumb: 'er-shell/images/dial-thumb.jpg',
         desc: 'Practice free flow singing with immediate feedback on your pitch.',
         href: '#freestyle',
         appUrl: 'er-apps/PitchDial/main.html',
-        aspectRatio: 5 / 6
+        aspectRatio: 5 / 6,
+        show:true
       }, {
         name: 'UpOrDown',
         thumb: 'er-shell/images/upordown.jpg',
         desc: 'Can you tell if the second node higher or lower than the first note?',
         href: '#upordown',
         appUrl: 'er-apps/UpOrDown/main.html',
-        aspectRatio: 9/10
+        aspectRatio: 9/10,
+        show:true
       }, {
         name: 'MelodyGraph',
         thumb: 'er-shell/images/melodygraph.jpg',
         desc: 'Get familiar with ups and downs in the songs.',
         href: '#melodygraph',
         appUrl: 'er-apps/MelodyGraph/main.html',
-        aspectRatio: 9/10
+        aspectRatio: 9/10,
+        show:true
       }, {
         name: 'SwarSpace',
         thumb: 'er-shell/images/swarspace.jpg',
         desc: 'Develop a sense of space between two notes - how far apart they are?',
         href: '#swarspace',
         appUrl: 'er-apps/SwarSpace/main.html',
-        aspectRatio: 9/10
+        aspectRatio: 9/10,
+        show:true
       }];
       appsInfo.selectedIndex = -1;
       appsInfo.setSelected = function(index) {
