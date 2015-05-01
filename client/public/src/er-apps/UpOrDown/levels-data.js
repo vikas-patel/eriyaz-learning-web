74e45e658246f13079037e9628ec8ebe30a15a96
Ashwin Jain <ashvyn.jain@gmail.com>
2015-05-01 10:56:44 +0530

    up or down improve colors, app thumbnail

--- client/public/src/er-apps/UpOrDown/levels-data.js
+++ client/public/src/er-apps/UpOrDown/levels-data.js
@@ -1,4 +1,8 @@
 define([],function() {
+	var purple = "#8f6da5";
+	var blue = "#308ec1";
+	var green = "#1de2a4";
+	var red = "#ff6b77";
 	return {
 		interval : {
 			XL : [900,1500],
@@ -12,70 +16,70 @@ define([],function() {
 				interval : "XL",
 				numNotes : 2,
 				testNotes : [1,2],
-				color:"lightblue"
+				color:blue
 			},
 			{
 				desc : "large interval",
 				interval : "L",
 				numNotes : 2,
 				testNotes : [1,2],
-				color:"lightgreen"
+				color:purple
 			},
 			{
 				desc : "medium interval",
 				interval : "M",
 				numNotes : 2,
 				testNotes : [1,2],
-				color : "green"
+				color : red
 			},
 			{
 				desc : "small interval",
 				interval : "S",
 				numNotes : 2,
 				testNotes : [1,2],
-				color: "red"
+				color: green
 			},
 			{
 				desc : "medium interval between note2 & note3",
 				interval : "M",
 				numNotes : 3,
 				testNotes : [2,3],
-				color: "green"
+				color: red
 			},
 			{
 				desc : "small interval between note2 & note3",
 				interval : "S",
 				numNotes : 3,
 				testNotes : [2,3],
-				color : "red"
+				color : green
 			},
 			{
 				desc : "medium interval between note1 & note2",
 				interval : "M",
 				numNotes : 3,
 				testNotes : [1,2],
-				color: "green"
+				color: red
 			},
 			{
 				desc : "small interval between note1 & note2",
 				interval : "S",
 				numNotes : 3,
 				testNotes : [1,2],
-				color: "red"
+				color: green
 			},
 			{
 				desc : "medium interval between note1 & note3",
 				interval : "M",
 				numNotes : 3,
 				testNotes : [1,3],
-				color : "green"
+				color : red
 			},
 			{
 				desc : "small interval between note1 & note3",
 				interval : "S",
 				numNotes : 3,
 				testNotes : [1,3],
-				color: "red"
+				color: green
 			}
 		]
 	};
