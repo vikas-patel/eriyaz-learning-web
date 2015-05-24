01654405f3bfd5dc432a9e5f73771488a1707416
Vikas Patel <v.vikas.patel@gmail.com>
2015-05-24 21:53:57 +0530

    swarPosition:reverting levels

--- client/public/src/er-apps/SwarPosition/levels.js
+++ client/public/src/er-apps/SwarPosition/levels.js
@@ -2,22 +2,22 @@ define([], function() {
 	return [{
 			name: "Level 1",
 			direction: "asc",
-			notes: [4, 7, 11],
+			notes: [2, 7, 12],
 			total: 10
 		}, {
 			name: "Level 2",
 			direction: "asc",
-			notes: [2, 5, 9, 12],
+			notes: [2, 4, 7, 12],
 			total: 10
-		}, {
+		} , {
 			name: "Level 3",
-			direction: "desc",
-			notes: [9, 5, 2],
+			direction: "asc",
+			notes: [2, 4, 5, 7, 12],
 			total: 10
 		}, {
 			name: "Level 4",
-			direction: "desc",
-			notes: [11, 7, 4, 0],
+			direction: "asc",
+			notes: [2, 4, 5, 7, 9, 12],
 			total: 10
 		}, {
 			name: "Level 5",
@@ -26,6 +26,26 @@ define([], function() {
 			total: 10
 		}, {
 			name: "Level 6",
+			direction: "desc",
+			notes: [11, 7, 0],
+			total: 10
+		}, {
+			name: "Level 7",
+			direction: "desc",
+			notes: [11, 9, 7, 0],
+			total: 10
+		},{
+			name: "Level 8",
+			direction: "desc",
+			notes: [11, 9, 7, 5, 0],
+			total: 10
+		}, {
+			name: "Level 9",
+			direction: "asc",
+			notes: [11, 9, 7, 5, 2, 0],
+			total: 10
+		}, {
+			name: "Level 10",
 			direction: "desc",
 			notes: [11, 9, 7, 5, 4, 2, 0],
 			total: 10
