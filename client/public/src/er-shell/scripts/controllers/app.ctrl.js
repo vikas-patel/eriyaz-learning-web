f71f7789b7428a76123d8d0324569706de961737
Vikas Patel <v.vikas.patel@gmail.com>
2015-06-23 17:13:24 +0530

    unwind shortcuts on app change

--- client/public/src/er-shell/scripts/controllers/app.ctrl.js
+++ client/public/src/er-shell/scripts/controllers/app.ctrl.js
@@ -4,13 +4,17 @@
   		$scope.loginSignupDialogModel = LoginSignupDialogModel.loginSignupDialogModel;
   		$scope.appsInfo = AppsInfoModel;
   		$rootScope.$watch(function() {return AppsInfoModel.selectedIndex}, function() {
-  				if (AppsInfoModel.selectedIndex < 0) return;
+  				if (AppsInfoModel.selectedIndex < 0) {
+  					hotkeys.purgeHotkeys();
+  					return;
+  				}
                 var keys = AppsInfoModel.apps[AppsInfoModel.selectedIndex].hotkeys;
                 if (!keys) return;
                 angular.forEach(keys, function (key) {
                 	hotkeys.add({
                     combo: key[0],
                     description: '',
+                    persistent: false,
                     callback: function() {
                         var scope = angular.element($('#selected-app').children().first()).scope();
                         scope.$eval(key[1]);
