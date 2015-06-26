3246acd2d96482f3b817d117c976f23e72a998c5
Vikas Patel <v.vikas.patel@gmail.com>
2015-06-26 16:02:07 +0530

    change levels

--- client/public/src/er-apps/VoiceMatch/levels.js
+++ client/public/src/er-apps/VoiceMatch/levels.js
@@ -2,32 +2,32 @@ define([], function() {
 	return [{
 			name: "Level 1",
 			isBeepPersistent: true,
-			notes: [0, 2],
+			notes: [0, 2, 4],
 			total: 10
 		}, {
 			name: "Level 2",
 			isBeepPersistent: true,
-			notes: [0, 2, 4, 5, 7],
+			notes: [0, 2, 4, 5, 7, 9, 11, 12],
 			total: 10
 		},{
 			name: "Level 3",
 			isBeepPersistent: true,
-			notes: [0, 2, 4, 5, 7, 9, 11, 12],
+			notes: [-5, -3, -1, 0, 2, 4, 5, 7, 9, 11, 12],
 			total: 10
 		},{
 			name: "Level 4",
 			isBeepPersistent: false,
-			notes: [0, 2],
+			notes: [0, 2, 4],
 			total: 10
 		}, {
 			name: "Level 5",
 			isBeepPersistent: false,
-			notes: [0, 2, 4, 5, 7],
+			notes: [0, 2, 4, 5, 7, 9, 11, 12],
 			total: 10
 		},{
 			name: "Level 6",
 			isBeepPersistent: false,
-			notes: [0, 2, 4, 5, 7, 9, 11, 12],
+			notes: [-5, -3, -1, 0, 2, 4, 5, 7, 9, 11, 12],
 			total: 10
 		}
 	];
