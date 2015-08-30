465f2ce0bf7bf48c81e771d9de729c7a53789531
Vikas Patel <v.vikas.patel@gmail.com>
2015-08-30 12:08:12 +0530

    more levels

--- client/public/src/er-apps/SargamMemorizer/levels.js
+++ client/public/src/er-apps/SargamMemorizer/levels.js
@@ -11,16 +11,40 @@ define([], function() {
 			cents: 50,
 			notes: [2, 4, 5, 7, 9, 11, 12],
 			total: 10
-		} , {
+		} ,{
 			name: "Level 3",
+			direction: "asc",
+			cents: 30,
+			notes: [2, 4, 5, 7, 9, 11, 12],
+			total: 10
+		} ,{
+			name: "Level 4",
+			direction: "asc",
+			cents: 20,
+			notes: [2, 4, 5, 7, 9, 11, 12],
+			total: 10
+		} , {
+			name: "Level 5",
 			direction: "desc",
 			cents: 100,
 			notes: [11, 9, 7, 5, 4, 2, 0],
 			total: 10
 		}, {
-			name: "Level 4",
+			name: "Level 6",
 			direction: "desc",
 			cents: 50,
+			notes: [11, 9, 7, 5, 4, 2, 0],
+			total: 10
+		}, {
+			name: "Level 7",
+			direction: "desc",
+			cents: 30,
+			notes: [11, 9, 7, 5, 4, 2, 0],
+			total: 10
+		}, {
+			name: "Level 8",
+			direction: "desc",
+			cents: 20,
 			notes: [11, 9, 7, 5, 4, 2, 0],
 			total: 10
 		}
