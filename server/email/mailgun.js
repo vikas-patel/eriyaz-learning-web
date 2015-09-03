2277cf795e4cbc4d0dd528c6eadefa0395e76002
ankur_patil <ankur.patil@gmail.com>
2015-09-03 10:21:34 +0530

    SendEmail works only from production environment

--- server/email/mailgun.js
+++ server/email/mailgun.js
@@ -1,5 +1,6 @@
 var nodemailer = require('nodemailer');
 var mg = require('nodemailer-mailgun-transport');
+var config = require('../env').config();
 
 var smtpTransport = nodemailer.createTransport(mg({auth:{
 	api_key: 'key-c9b4ec5b9599a6853a521a848ecbc695',
@@ -8,6 +9,8 @@ var smtpTransport = nodemailer.createTransport(mg({auth:{
  }));
 
 exports.SendEMail = function(mailOptions, done){
+	if(config.env == "production")
+	{
       smtpTransport.sendMail(mailOptions, function(err) {
 		if(err)
 			console.log('error','Error while sending mail');
@@ -16,4 +19,10 @@ exports.SendEMail = function(mailOptions, done){
 		if(done)
 			done(err, 'done');
       });
+	}
+	else
+		console.log("Email will be sent from production environment only");
+		
+	if(done)
+		done(null, 'done');
 }
