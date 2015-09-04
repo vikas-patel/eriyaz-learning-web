e4c39c34b3b8945f525281c98321a1a0cf7b825b
ankur_patil <ankur.patil@gmail.com>
2015-09-04 10:23:46 +0530

    Changed email contents

--- server/email/notifier.js
+++ server/email/notifier.js
@@ -5,14 +5,53 @@ var admin_email_id = "eriyazonline@gmail.com"
 
 exports.SendWelcomeEmail = function(email_id,name){
 	var welcomeSubject = "Welcome to eRiyaz"
-	var welcomeText = "Greetings "+name+"!\n\nWelcome to eRiyaz. Now you can learn to sing 10X faster by logging in to www.eriyaz.com using your email id "+email_id+".\n\nRegards,\neRiyaz Team"
+	var welcomeText = ""
+	welcomeText+="Greetings "+name+",\n"
+	welcomeText+="\n"
+	welcomeText+="Welcome to eRiyaz.\n"
+	welcomeText+="Your account is pending for admin approval. You will receive a confirmation email on account activation.\n"
+	welcomeText+="\n"
+	welcomeText+="Regards,\n"
+	welcomeText+="eRiyaz Team,\n"
+	welcomeText+="eriyazonline@gmail.com\n"
+	welcomeText+="+91 8287459406"
+
 	var email = {from:from, to:email_id, subject:welcomeSubject, text:welcomeText}
 	mailgun.SendEMail(email);
 }
 
 exports.NewUserEmail = function(user){
 	var welcomeSubject = "New user on eRiyaz"
-	var welcomeText = "Hello Admin!\n\nFollowing user has signed up on eRiyaz:\nEmail:"+user.local.email+"\nName:"+user.name+"\nDoB:"+user.dob+"\nGender:"+user.gender+"\nPhone:"+user.phone+"\n\nRegards,\neRiyaz Team"
+	var welcomeText = ""
+	welcomeText+="Hello Admin!\n"
+	welcomeText+="\n"
+	welcomeText+="Following user has signed up on eRiyaz:\n"
+	welcomeText+="Email:\t"+user.local.email+"\n"
+	welcomeText+="Name:\t"+user.name+"\n"
+	welcomeText+="DoB:\t"+user.dob+"\n"
+	welcomeText+="Gender\t: "+user.gender+"\n"
+	welcomeText+="Phone:\t"+user.phone+"\n"
+	welcomeText+="\n"
+	welcomeText+="Regards,\n"
+	welcomeText+="eRiyaz Team"
 	var email = {from:from, to:admin_email_id, subject:welcomeSubject, text:welcomeText}
+	mailgun.SendEMail(email);
+}
+
+exports.SendActivationEmail = function(email_id,name){
+	var welcomeSubject = "eRiyaz Account Activated"
+	var welcomeText= ""
+	welcomeText+="Greetings "+name+",\n"
+	welcomeText+="\n"
+	welcomeText+="Thank you for joining eRiyaz.\n"
+	welcomeText+="\n"
+	welcomeText+="Your account has been activated. You can login with your email/password now.\n"
+	welcomeText+="\n"
+	welcomeText+="Regards,\n"
+	welcomeText+="eRiyaz Team,\n"
+	welcomeText+="eriyazonline@gmail.com\n"
+	welcomeText+="+91 8287459406"
+
+	var email = {from:from, to:email_id, subject:welcomeSubject, text:welcomeText}
 	mailgun.SendEMail(email);
 }
