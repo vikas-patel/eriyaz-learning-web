d975abe8edb3ec79dd5620612df2a0d3790cbec4
Vikas Patel <v.vikas.patel@gmail.com>
2015-09-21 11:24:45 +0530

    removed levels with note different of 20 cents

--- client/public/src/er-apps/SargamTuner/levels.js
+++ client/public/src/er-apps/SargamTuner/levels.js
@@ -17,59 +17,47 @@ define([], function() {
 			cents: 30,
 			notes: [2, 4, 5, 7, 9, 11, 12],
 			total: 10
-		} ,{
-			name: "Level 4",
-			direction: "asc",
-			cents: 20,
-			notes: [2, 4, 5, 7, 9, 11, 12],
-			total: 10
 		} , {
-			name: "Level 5",
+			name: "Level 4",
 			direction: "desc",
 			cents: 100,
+			notes: [11, 9, 7, 5, 4, 2, 0],
+			total: 10
+		}, {
+			name: "Level 5",
+			direction: "desc",
+			cents: 50,
 			notes: [11, 9, 7, 5, 4, 2, 0],
 			total: 10
 		}, {
 			name: "Level 6",
 			direction: "desc",
-			cents: 50,
-			notes: [11, 9, 7, 5, 4, 2, 0],
-			total: 10
-		}, {
-			name: "Level 7",
-			direction: "desc",
 			cents: 30,
 			notes: [11, 9, 7, 5, 4, 2, 0],
 			total: 10
 		}, {
-			name: "Level 8",
-			direction: "desc",
-			cents: 20,
-			notes: [11, 9, 7, 5, 4, 2, 0],
-			total: 10
-		}, {
-			name: "Level 9",
+			name: "Level 7",
 			direction: "asc",
 			isDiscrete: true,
 			cents: 100,
 			notes: [2, 4, 5, 7, 9, 11, 12],
 			total: 10
 		}, {
-			name: "Level 10",
+			name: "Level 8",
 			direction: "asc",
 			isDiscrete: true,
 			cents: 50,
 			notes: [2, 4, 5, 7, 9, 11, 12],
 			total: 10
 		}, {
-			name: "Level 11",
+			name: "Level 9",
 			direction: "desc",
 			isDiscrete: true,
 			cents: 100,
 			notes: [11, 9, 7, 5, 4, 2, 0],
 			total: 10
 		}, {
-			name: "Level 12",
+			name: "Level 10",
 			direction: "desc",
 			isDiscrete: true,
 			cents: 50,
